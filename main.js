f718146868897745845e490a28624349785b455f
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-26 12:38:36 +0100

    established successful connection to database

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-import password from './password';
-
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
      host: 'localhost', 
      user:'nodeserver', 
-     password: password,
+     password: 'nodeserver',
      connectionLimit: 5
 });
 
 pool.getConnection()
     .then(conn => { 
+      conn.query("use githubaccess")
+          .then((whatIsReturned) => {
+            console.log(whatIsReturned);
+          })
+          .catch(err => {
+          //output error to console
+          console.log(err);
+          conn.end();
+      })
+    })
+    .catch(err => {
+      //unable to esablish connection
+      console.log("Unable to esablish connection to db");
+      console.log(err);
     })

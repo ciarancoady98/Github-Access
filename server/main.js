5817f4d5b19b565daa602fb18a477e4b272f6f41
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-26 16:39:49 +0100

    added some sample code related to console input

--- server/main.js
+++ server/main.js
@@ -1,5 +1,15 @@
 const password = require('./password');
 const mariadb = require('mariadb');
+const github = require('octonode');
+var standard_input = process.stdin;
+
+
+
+
+/*
+  Setup a connection to the database to store
+  the results of our queries
+*/
 const pool = mariadb.createPool({
      host: 'localhost', 
      user:'nodeserver', 
@@ -23,4 +33,51 @@ pool.getConnection()
       //unable to esablish connection
       console.log("Unable to esablish connection to db");
       console.log(err);
-    })
+    });
+
+
+
+
+/*
+    Setup a client so we can query the github rest api
+    Currently no authentication
+*/
+/*
+var client = github.client({
+  username: 'user',
+  password: 'password'
+});
+*/
+var client = github.client();
+client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
+client.limit(function (err, left, max, reset) {
+  console.log(left); // 4999
+  console.log(max);  // 5000
+  console.log(reset);  // 1372700873 (UTC epoch seconds)
+});
+
+
+
+
+/*
+  Handling user input from the console
+*/
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+// Prompt user to input data in console.
+console.log("Please input text in command line.");
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else
+    {
+        // Print user input in console.
+        console.log('User Input Data : ' + data);
+    }
+});

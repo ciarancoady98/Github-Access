7264a0c9b05a0420ba472d0d955f897e0fddb1f0
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-13 13:27:25 +0000

    removed disused password.js files

--- server/main.js
+++ server/main.js
@@ -5,12 +5,12 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 
 
+//Mainline
 console.log(
   chalk.yellow(
     figlet.textSync('Github Access', { horizontalLayout: 'full' })
   )
 );
-
 const inquirer  = require('./src/inquirer');
 const run = async () => {
   const gitCredentials = await inquirer.askGithubCredentials();
@@ -20,34 +20,3 @@ const run = async () => {
 };
 run();
 
-/*
-    Setup a client so we can query the github rest api
-    Currently no authentication
-*/
-/*
-Build a client that has access to public data
-
-var client = github.client();
-client.get('/users/pksunkara', {}, function (err, status, body, headers) {
-  console.log(body); //json object
-});
-*/
-/*
-Build a client from an access token
-
-var client = github.client('someaccesstoken');
-
-client.get('/user', {}, function (err, status, body, headers) {
-  console.log(body); //json object
-});
-
-*/
-
-/*
-build a client from credentials
-
-var client = github.client({
-  username: 'pksunkara',
-  password: 'password'
-});
-*/
--- server/password_template.js
+++
@@ -1,2 +0,0 @@
-let password = '';
-module.exports = password;

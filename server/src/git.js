7f041861a6f1d93f3cce64b3e479ee933a452578
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-13 11:51:59 +0000

    added git.js which will handle all calls to the github api

---
+++ server/src/git.js
@@ -0,0 +1,31 @@
+/*
+    Setup a client so we can query the github rest api
+    Currently no authentication
+*/
+/*
+Build a client that has access to public data
+
+var client = github.client();
+client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
+*/
+/*
+Build a client from an access token
+
+var client = github.client('someaccesstoken');
+
+client.get('/user', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
+
+*/
+
+/*
+build a client from credentials
+
+var client = github.client({
+  username: 'pksunkara',
+  password: 'password'
+});
+*/

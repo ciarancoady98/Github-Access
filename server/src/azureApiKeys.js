523fb431f03f1b05ab64ccf0bdffb70583c95ce0
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-24 21:56:12 +0000

    began setup for azure text analysis

---
+++ server/src/azureApiKeys.js
@@ -0,0 +1,2 @@
+export let endpoint = "test";
+export let key = "test";
---
+++ server/src/textAnalysis.js
@@ -0,0 +1,6 @@
+const azureApiKeys = require("./azureApiKeys");
+const os = require("os");
+const CognitiveServicesCredentials = require("@azure/ms-rest-js");
+const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
+
+console.log(azureApiKeys.key);
